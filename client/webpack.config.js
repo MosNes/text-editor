9e496234fbef9f0966eab7499402f2f890f2ffb2
MosNes <7hekarl@gmail.com>
2022-09-04 09:38:36 -0400

    added manifest plugin to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,20 +15,41 @@ module.exports = () => {
 			path: path.resolve(__dirname, 'dist'),
 		},
 		plugins: [
-      //HTML plugin to generate index.html in dist folder
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin'
-      }),
-      //workbox plugin to set up service worker
-      new InjectManifest({
-        //points to the service worker config file
-        swSrc: './src-sw.js',
-        //sets the name of the service worker file generated in dist folder
-        swDest: 'service-worker.js'
-      })
-
-    ],
+			//HTML plugin to generate index.html in dist folder
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				title: 'Webpack Plugin',
+			}),
+			//workbox plugin to set up service worker
+			new InjectManifest({
+				//points to the service worker config file
+				swSrc: './src-sw.js',
+				//sets the name of the service worker file generated in dist folder
+				swDest: 'service-worker.js',
+			}),
+			new WebpackPwaManifest({
+				name: 'Just Another Text Editor',
+				short_name: 'JATE',
+				description: 'A browser based text editor',
+				background_color: '#7eb4e2',
+				theme_color: '#7eb4e2',
+				start_url: './',
+				publicPath: './',
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+					{
+						src: path.resolve('src/images/logo.png'),
+						size: '1024x1024',
+						destination: path.join('assets', 'icons'),
+						purpose: 'maskable',
+					},
+				],
+			}),
+		],
 
 		module: {
 			rules: [
