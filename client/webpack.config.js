87e2111301019d3809b039624d146e8e2e94322f
MosNes <7hekarl@gmail.com>
2022-09-04 08:33:24 -0400

    added webpack plugins to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
 	return {
 		mode: 'development',
@@ -17,6 +15,18 @@ module.exports = () => {
 			path: path.resolve(__dirname, 'dist'),
 		},
 		plugins: [
+      //HTML plugin to generate index.html in dist folder
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+      //workbox plugin to set up service worker
+      new InjectManifest({
+        //points to the service worker config file
+        swSrc: './src/src-sw.js',
+        //sets the name of the service worker file generated in dist folder
+        swDest: 'service-worker.js'
+      })
 
     ],
 
