d6444fa54dd7a820f35b3ebbd7f4b76dd6e3147f
MosNes <7hekarl@gmail.com>
2022-09-04 08:39:20 -0400

    added code for getDb function in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,24 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  //create db connection
+  const jateDb = await openDB('jate', 1);
+  
+  //create a transaction and specify object store and scope
+  const tx = jateDb.transaction('jate', 'readonly');
+  
+  //open the chosen object store
+  const store = tx.objectStore('jate');
+  
+  //use getAll() method to get all info in the table
+  const request = store.getAll();
+  
+  //get result/confirmation
+  const result = await request;
+  console.log('result.value',result);
+
+  return result;
+}
 
 initdb();
