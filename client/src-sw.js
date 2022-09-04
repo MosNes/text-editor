793235f77a40837c14dfd8663b6920ab4473ff5e
MosNes <7hekarl@gmail.com>
2022-09-04 09:31:24 -0400

    added image caching to src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 // image caching
 registerRoute(
   ({request}) => request.destination === 'image',
